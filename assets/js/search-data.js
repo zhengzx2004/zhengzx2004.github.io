e194c4a24281568588a307516989336fdf50c828
zhengzx2004 <zhengzx2004@gmail.com>
2025-08-06 09:22:26 +0000

    Deploying to gh-pages from @ zhengzx2004/zhengzx2004.github.io@99b94eaec8940e48531f2f85939882deece01461 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
